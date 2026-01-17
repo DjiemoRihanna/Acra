8e1e2730d5f2fca494f39111f77fd04b25e4ba35
kwandji <kwandji@irex.aretex.ca>
2026-01-17 03:42:29 -0500

    mise a jour de la phase 1: ajout du tableau de bord, de la page de preinitialisation et de connexion avec leurs logique3

--- src/auth/decorators.py
+++ src/auth/decorators.py
@@ -3,16 +3,23 @@ from flask import abort
 from flask_login import current_user
 from src.models import UserRole
 
-def role_required(allowed_roles):
-    """Vérifie si current_user.role (Enum) est dans allowed_roles (liste d'Enums)."""
+def role_required(*allowed_roles): 
+    """
+    Vérifie si current_user.role est parmi les rôles autorisés.
+    L'usage de *allowed_roles permet de passer un ou plusieurs arguments.
+    Exemple: @role_required(UserRole.ADMIN) 
+    OU @role_required(UserRole.ADMIN, UserRole.ANALYST_SENIOR)
+    """
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             if not current_user.is_authenticated:
                 abort(401)
-            # On vérifie si l'objet Enum de l'utilisateur est dans la liste
+            
+            # allowed_roles est maintenant un tuple (itérable)
             if current_user.role not in allowed_roles:
                 abort(403)
+                
             return f(*args, **kwargs)
         return decorated_function
     return decorator
